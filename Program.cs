a9951660d4c44a145a727d95bf521c9d5d611213
Diegowrl <diegowrl@gmail.com>
2022-05-22 19:54:05 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+
+Initial();
+
+static void Initial()
+{
+
+    string jsonString = File.ReadAllText(@"D:\myFile.json");
+
+    var response = JsonConvert.DeserializeObject<List<TicketModel>>(jsonString);
+
+    foreach (var item in response)
+    {
+        Console.WriteLine($"Operacaçao: {item.operation} , Unit-Cost: {item.unitCost} ,Quantidade:{item.quantity}");
+    }
+
+
+}
+
+public class TicketModel
+{
+    [JsonProperty("operation")]
+    public string operation { get; set; }
+
+    [JsonProperty("unit-cost")]
+    public decimal unitCost { get; set; }
+
+    [JsonProperty("quantity")]
+    public int quantity { get; set; }
+}
